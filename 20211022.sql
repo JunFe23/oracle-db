64cd3786e5d13e1cd552784289c72c46fc3c213e
JunFe23 <ogum0405@hanmail.net>
2021-10-23 23:47:18 +0900

    2021.10.23

--- 20211022.sql
+++ 20211022.sql
@@ -1,2 +1,8 @@
 SELECT DECODE(GENDER, '남성', 1, 2) FROM DUAL;
 
+SELECT DECODE(SUBSTR(PHONE,1,3),'011','SK','016','KT','기타') FROM MEMBERS;
+
+SELECT DECODE(SUBSTR(PHONE, 1, 3),
+                        '011', 'SK',
+                        '016', 'KT',
+                        '기타') FROM MEMBERS;
